9ad904d564c69e5fcc5ac6d9641cf3405bc22f96
Felipe <antoniofelipesilva@live.com>
2023-06-02 18:34:17 -0300

    add express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Felipe')
+import express, { Request, Response } from "express";
+
+const server = express();
+
+server.get('/', (req: Request, res: Response) => {
+  res.send('Servidor Online!')
+})
+
+server.get('/home', (req: Request, res: Response) => {
+  res.send('Pagina home!')
+})
+
+server.listen(3000, ()=>{console.log('Servidor Online!')})
